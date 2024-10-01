f6bf19a921976d2d8c6b113f6ecb0dabe10ae167
Roberto Garcia <jorogarverri@gmail.com>
2024-10-01 11:53:58 -0600

    Form to complete user information

---
+++ app/src/main/java/com/optic/deliverykotlinudemy/activities/SaveImageActivity.kt
@@ -0,0 +1,12 @@
+package com.optic.deliverykotlinudemy.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.optic.deliverykotlinudemy.R
+
+class SaveImageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_image)
+    }
+}
